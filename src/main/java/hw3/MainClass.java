4a4ef1ec1fef2f6934b542518ea85843c173727a
Ilya <KobyzevIlya@gmail.com>
2023-06-15 21:58:11 +0300

    docker_test_27

--- src/main/java/hw3/MainClass.java
+++ src/main/java/hw3/MainClass.java
@@ -4,11 +4,11 @@ import hw3.calculator.AgeCalculator;
 
 public class MainClass {
     public static void main(String[] args) {
-        AgeCalculator ageCalculator = new AgeCalculator();
-        ageCalculator.analyze();
+        // AgeCalculator ageCalculator = new AgeCalculator();
+        // ageCalculator.analyze();
 
-        // for (int i = 0; i < 5; ++i) {
-        //     System.out.println("Hello world!");
-        // }
+        for (int i = 0; i < 5; ++i) {
+            System.out.println("Hello world!");
+        }
     }
 }
